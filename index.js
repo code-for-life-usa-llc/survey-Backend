9ac5f4be9659b393ac540703ec6f6dfd0925b848
Daniel <nordbydaniel@gmail.com>
2023-06-20 10:09:17 -0400

    schema folder + various updates

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config();
-const mongoString = process.env.DATABASE_URL
-mongoose.connect(mongoString)
-const database = mongoose.connection
+require("dotenv").config();
 
-database.on('error', () => {
-    console.log("connected")
-})
-const routes = require('./routes')
+const express = require("express");
+const mongoString = process.env.DATABASE_URL;
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", () => {
+  console.log("connected");
+});
+
 app = express();
-app.use('/api', routes)
+app.use(express.json());
+app.use("/api", routes);
 
-
-app.listen(300, ()=> {
-    console.log('server started!')
-})
+app.listen(3000, () => {
+  console.log("server started!");
+});
--- routes.js
+++ routes.js
@@ -1,26 +1,26 @@
-const Person = require("./person")
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Person = require("./schemas/person");
+
 //Post Method
-router.post('/post', async(req, res) => {
-    const data = new Person({
-        dateOfBirth: req.dateOfBirth,
-        grade: req.grade,
-        school: req.school,
-        gender: req.gender,
-        zipCode: req.zipCode,
-        borough: req.borough,
-        ethnicity: req.ethnicity,
-        language: req.language,
-    })
+router.post("/post", async (req, res) => {
+  const data = new Person({
+    dateOfBirth: req.body.dateOfBirth,
+    grade: req.body.grade,
+    school: req.body.school,
+    gender: req.body.gender,
+    zipCode: req.body.zipCode,
+    borough: req.body.borough,
+    ethnicity: req.body.ethnicity,
+    language: req.body.language,
+  });
 
-    try {
-        const dataToSave = await data.save();
-        res.status(200).json(dataToSave)
-    }
-    catch (error) {
-        res.status(400).json({ message: error.message })
-    }
-})
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-module.exports = router
+module.exports = router;
