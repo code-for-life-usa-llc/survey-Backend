48525780c5bb73883af5777d30064efa4f90dc77
Daniel <nordbydaniel@gmail.com>
2023-06-20 13:36:16 -0400

    weird tracking issue

---
+++ netlify/functions/api.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const serverless = require("serverless-http");
+const mongoString = process.env.DATABASE_URL;
+const mongoose = require("mongoose");
+const routes = require("../../routes");
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+const app = express();
+
+database.on("error", console.log("Connection Error"));
+
+app.use(express.json());
+app.use("/api/", routes);
+
+mongoose.connection.close();
+
+export const handler = serverless(app);
--- routes.js
+++ routes.js
@@ -1,37 +1,26 @@
-// const express = require("express");
-// const router = express.Router();
-// const Person = require("./schemas/person");
+const express = require("express");
+const router = express.Router();
+const Person = require("./schemas/person");
 
-// //Post Method
-// router.post("/post", async (req, res) => {
-//   const data = new Person({
-//     dateOfBirth: req.body.dateOfBirth,
-//     grade: req.body.grade,
-//     school: req.body.school,
-//     gender: req.body.gender,
-//     zipCode: req.body.zipCode,
-//     borough: req.body.borough,
-//     ethnicity: req.body.ethnicity,
-//     language: req.body.language,
-//   });
+//Post Method
+router.post("/post", async (req, res) => {
+  const data = new Person({
+    dateOfBirth: req.body.dateOfBirth,
+    grade: req.body.grade,
+    school: req.body.school,
+    gender: req.body.gender,
+    zipCode: req.body.zipCode,
+    borough: req.body.borough,
+    ethnicity: req.body.ethnicity,
+    language: req.body.language,
+  });
 
-//   try {
-//     const dataToSave = await data.save();
-
-//     console.log("DATA:", dataToSave);
-//     res.status(200).json(dataToSave);
-//   } catch (error) {
-//     res.status(400).json({ message: error.message });
-//   }
-// });
-
-// module.exports = router;
-
-import express, { Router } from 'express';
-
-const api = express();
-
-const router = Router();
-router.post('/hello', (req, res) => res.send('Hello World!'));
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
