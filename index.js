30ca49e3482e3e556de595275d1194328161fbd4
Daniel <nordbydaniel@gmail.com>
2023-06-20 11:05:51 -0400

    configuring for Netlify

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const express = require("express");
+const serverless = require("serverless-http")
+const mongoString = process.env.DATABASE_URL;
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", () => {
+  console.log("connected");
+});
+
+app = express();
+app.use(express.json());
+app.use("/api", routes);
+
+export const handler = serverless(app);
+
+// app.listen(3000, () => {
+//   console.log("server started!");
+// });
