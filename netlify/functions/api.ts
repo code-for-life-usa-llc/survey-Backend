cd7c3b06ce3c030454246c367af95abe21822ab9
Daniel <nordbydaniel@gmail.com>
2023-06-20 13:22:34 -0400

    logging

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,21 +1,17 @@
 require("dotenv").config();
 
 const express = require("express");
-const serverless = require("serverless-http")
+const serverless = require("serverless-http");
 const mongoString = process.env.DATABASE_URL;
 const mongoose = require("mongoose");
 const routes = require("../../routes");
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
-
-database.on("error", () => {
-  console.log("Connection Error");
-});
-
-console.log("Database connection:", database)
-
 const app = express();
+
+database.on("error", console.log("Connection Error"));
+
 app.use(express.json());
 app.use("/api/", routes);
 
