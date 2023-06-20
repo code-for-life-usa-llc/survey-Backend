cc6eaff8d420eb654d4c232aa4ee436db45e8274
Daniel <nordbydaniel@gmail.com>
2023-06-20 13:43:15 -0400

    weird tracking issue

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,20 +1,32 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express = require("express");
-const serverless = require("serverless-http");
-const mongoString = process.env.DATABASE_URL;
-const mongoose = require("mongoose");
-const routes = require("../../routes");
+// const express = require("express");
+// const serverless = require("serverless-http");
+// const mongoString = process.env.DATABASE_URL;
+// const mongoose = require("mongoose");
+// const routes = require("../../routes");
 
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-const app = express();
+// mongoose.connect(mongoString);
+// const database = mongoose.connection;
+// const app = express();
 
-database.on("error", console.log("Connection Error"));
+// database.on("error", console.log("Connection Error"));
 
-app.use(express.json());
-app.use("/api/", routes);
+// app.use(express.json());
+// app.use("/api/", routes);
 
-mongoose.connection.close();
+// mongoose.connection.close();
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
+
+import express, { Router } from 'express';
+import serverless from 'serverless-http';
+
+const api = express();
+
+const router = Router();
+router.get('/hello', (req, res) => res.send('Hello World!'));
+
+api.use('/api/', router);
+
+export const handler = serverless(api);
