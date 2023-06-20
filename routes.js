f30dd751e737575ec4187a06049883419477c7f0
Daniel <nordbydaniel@gmail.com>
2023-06-20 13:56:52 -0400

    rewrite to wait for database

--- routes.js
+++ routes.js
@@ -17,7 +17,7 @@ router.post("/post", async (req, res) => {
 
   try {
     const dataToSave = await data.save();
-    res.status(200).json({ message: "SAVED"});
+    res.status(200).json(dataToSave);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
