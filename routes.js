28cffae276ca49c0c7e501488c75453b7b8dff89
Kevin Hernandez <kevbhernandez24@gmail.com>
2023-06-27 23:09:52 -0400

    syntax bug fixed

--- routes.js
+++ routes.js
@@ -61,10 +61,7 @@ router.post('/post', async(req, res) => {
     }
     const dataToSave = await data.save();
     res.status(200).json(dataToSave);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+  });
 
 //GET ALL Method
 router.get("/getAll", async (req, res) => {
